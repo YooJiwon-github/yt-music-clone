ef989b450be6aad076e1bdffa3c4033a8f8466ea
jiwon <jiwon0768@tukorea.ac.kr>
2024-04-12 22:14:53 +0900

    playlist

--- app/playlist/page.tsx
+++ app/playlist/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { getPlaylistById } from '@/lib/dummyData';
+import { getRandomElementFromArray } from '@/lib/utils';
+import { permanentRedirect } from 'next/navigation';
+import React from 'react';
+import HeaderBgChanger from '@/components/HeaderBgChanger';
+import PlayListHead from '@/components/PlayListHead';
+import PagePadding from '@/components/PagePadding';
 
-const page = (props: any) => {
-  console.log(props);
+interface PlayListPageProps {
+  searchParams: {
+    list:string;
+  };
+}
+
+const page = async (props: PlayListPageProps) => {
+  const playlist = await getPlaylistById(Number(props.searchParams.list));
+
+  if(!playlist) permanentRedirect("/");
+
+  const imageSrc = getRandomElementFromArray(playlist.songList)?.imageSrc;
+
+
+
   return (
-    <div>playlist {props.searchParams.list}</div>
+    <PagePadding>
+      <HeaderBgChanger imageSrc={imageSrc} />
+      <div className='mt-12'></div>
+      <PlayListHead playlist={playlist} />
+    </PagePadding>
   )
 }
 
